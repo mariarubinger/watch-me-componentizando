095e22d7212fea9d958c7c4c579ada0aaff35204
Maria Clara Rubinger <rubinger.maria@gmail.com>
2021-03-29 18:48:06 +0100

    Desafio finalizado

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+interface Genre {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+// criada no desafio
+interface SideBarProps {
+  genres: Genre[];
+  selectedGenreId: number;
+  onClickCategory: (id: number) => void;
+}
+
+export function SideBar({ genres, selectedGenreId, onClickCategory}: SideBarProps) {
+  return (
+    <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => onClickCategory(genre.id)} // mudei de handleClickButton para onClickCategory
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+    </nav>
+  )
 }
